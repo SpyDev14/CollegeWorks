fee3c0f327a0b663cfe49f608075b34fc06271b7
SpyDev14 <135313844+SpyDev14@users.noreply.github.com>
2026-03-07 13:58:36 +0400

    worksave

--- SecondaryCourseWorks/GlobalStaticMethods.cs
+++ SecondaryCourseWorks/GlobalStaticMethods.cs
@@ -1,16 +1,34 @@
-﻿namespace SecondaryCourseWorks;
+﻿using System.Numerics;
+namespace SecondaryCourseWorks;
 
 internal static class GlobalStaticMethods
 {
-	public static T[] GetRandomArray<T>(int length, T min, T max)
+	/// <summary>
+	/// Генерирует массив указанной длины со случайными значениями в заданном диапазоне.
+	/// Точность для больших чисел <b>не идеальная</b>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="length"></param>
+	/// <param name="min"></param>
+	/// <param name="max"></param>
+	/// <returns>Новый заполненный <see cref="Array"/> переданного размера.</returns>
+	/// <exception cref="ArgumentOutOfRangeException"><paramref name="length"/> < 0</exception>
+	/// <exception cref="ArgumentException"><paramref name="min"/> > <paramref name="max"/></exception>
+	public static T[] GetRandomArray<T>(int length, T min, T max) where T : INumber<T>
 	{
-# error TODO
-		var rnd = new Random();
+		if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
+		if (min > max) throw new ArgumentException("min must be <= max");
+
+		double dMin = double.CreateTruncating(min);
+		double dMax = double.CreateTruncating(max);
+		double range = dMax - dMin;
+
+		var rnd = Random.Shared;
 		var array = new T[length];
 
 		for (int i = 0; i < length; i++)
-			array[i] = rnd.Next(min, max);
-
+			array[i] = T.CreateTruncating(dMin + rnd.NextDouble() * range);
+		
 		return array;
 	}
 
